5645a5908ec7c00a4299176bc1f0d3a892f69058
Mohamed-Kamel <muhamed.kamel.elsayed@gmail.com>
2017-03-31 21:14:54 +0200

    Designing and implementing DB with relationships in the models

---
+++ database/migrations/2017_03_31_182310_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name");
+            $table->text("details");
+            $table->float("price", 10, 2);
+            $table->float("highest_price", 10, 2);
+            $table->integer("no_of_bids");
+            $table->enum("online", ["yes", "no"]);
+            $table->string("image");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+<h1 style="text-align: center;">MAZAD</h1>
+
+<form action="/search" class="col-md-8 col-md-offset-2">
+
+    <div class="form-group">
+        <div class="col-md-7">
+            <input class="form-control" placeholder="Search by name" type="text" name="search" id="search">
+        </div>
+        
+        <button class="btn btn-default col-md-2" name="search">Search</button>
+    </div>
+
+</form>
+
+table>thead>tr>(th*6)&tbody>(tr>(td*6)*6)
+
+@stop
